b5e8e563caf2df338ba7a12d98e4ebdc186bb5ee
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-12 16:49:16 -0400

    added search input to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,44 @@
 import React from "react";
 import Image from "next/image";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png"
-            className="object-contain"
-            alt="header-logo"
-            fill={true}
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
-            className="object-contain"
-            alt="header-logo"
-            fill={true}
-          />
-        </div>
-
-        <h1>Right Side</h1>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png"
+          className="object-contain"
+          alt="header-logo"
+          fill={true}
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
+          className="object-contain"
+          alt="header-logo"
+          fill={true}
+        />
       </div>
 
       {/* Middle */}
 
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
+
       {/* Right */}
+
+      <h1>Right Side</h1>
     </div>
   );
 }
