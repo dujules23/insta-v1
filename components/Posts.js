014ed207dc2b9832f9fcda5f034b43de6a8e306c
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-18 23:08:18 -0400

    created header and image sections for the post component

--- components/Posts.js
+++ components/Posts.js
@@ -7,7 +7,7 @@ export default function Posts() {
       id: "1",
       username: "codewithpercy",
       userImg:
-        "https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png",
+        "https://rstatic.stores.musicarts.com/locations/durrell-j-2-mac-columbia---sc-5f82b5ac-3cb4-474f-b148-0c8cfb143d72.jpg",
       img: "https://images.unsplash.com/photo-1687057217908-54f8e6d30e3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
       caption: "Nice picture!",
     },
@@ -15,7 +15,7 @@ export default function Posts() {
       id: "1",
       username: "percivalwright",
       userImg:
-        "https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png",
+        "https://rstatic.stores.musicarts.com/locations/durrell-j-2-mac-columbia---sc-5f82b5ac-3cb4-474f-b148-0c8cfb143d72.jpg",
       img: "https://images.unsplash.com/photo-1610880846497-7257b23f6138?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
       caption: "New whip.",
     },
