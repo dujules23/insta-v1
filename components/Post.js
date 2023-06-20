5688b5f35c62530ff047537bae1a2ae7cd433ad2
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-19 23:55:27 -0400

    added caption and input box to the Post componenet

--- components/Post.js
+++ components/Post.js
@@ -5,6 +5,7 @@ import {
   HeartIcon,
   BookmarkIcon,
   ChatBubbleOvalLeftIcon,
+  FaceSmileIcon,
 } from "@heroicons/react/24/outline";
 
 export default function Post({ img, userImg, caption, username, id }) {
@@ -34,6 +35,24 @@ export default function Post({ img, userImg, caption, username, id }) {
         </div>
         <BookmarkIcon className="btn" />
       </div>
+
+      {/* Post Comments */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+
+      {/* Post Input Box */}
+
+      <form className="flex items-center p-4">
+        <FaceSmileIcon className="h-7" />
+        <input
+          className="border-none flex-1 focus:ring-0"
+          type="text"
+          placeholder="Enter your comment."
+        />
+        <button className="text-blue-400 fort-bold">Post</button>
+      </form>
     </div>
   );
 }
