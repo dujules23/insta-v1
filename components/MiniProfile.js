2452e07684b99540cb9005a5e20284c631479479
Durrell Jules <dujules23@gmail.com>
2023-10-09 22:54:20 -0400

    modified mini profile and story component to include the session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,22 @@
+import { useSession, signOut } from "next-auth/react";
+
 export default function MiniProfile() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src="https://rstatic.stores.musicarts.com/locations/durrell-j-2-mac-columbia---sc-5f82b5ac-3cb4-474f-b148-0c8cfb143d72.jpg"
+        src={session?.user.image}
         alt="user-image"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">codewithpercy</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button className="font-semibold text-blue-400 text-sm">Sign Out</button>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+        Sign Out
+      </button>
     </div>
   );
 }
--- components/Stories.js
+++ components/Stories.js
@@ -1,10 +1,12 @@
 import minifaker from "minifaker";
 import "minifaker/locales/en";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 import { useState, useEffect } from "react";
 
 export default function Stories() {
+  const { data: session } = useSession();
   const [storyUsers, setStoryUsers] = useState([]);
 
   useEffect(() => {
@@ -19,6 +21,13 @@ export default function Stories() {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border-1 overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img={session?.user.image}
+          username={session?.user.username}
+          isUser="true"
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
