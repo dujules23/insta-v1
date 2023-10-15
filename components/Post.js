f536cf936095be569f1ebc2ff7f01d9566805a8d
Durrell Jules <dujules23@gmail.com>
2023-10-14 23:20:52 -0400

    added the number of likes under like button

--- components/Post.js
+++ components/Post.js
@@ -123,6 +123,9 @@ export default function Post({ img, userImg, caption, username, id }) {
 
       {/* Post Comments */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} like(s)</p>
+        )}
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
