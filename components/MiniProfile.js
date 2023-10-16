92dbc9f2340cc71dab1c3467b585b267df30eabf
Durrell Jules <dujules23@gmail.com>
2023-10-15 23:59:33 -0400

    restructured Post.js and MiniProfile.js to use firebase auth

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,20 +1,31 @@
-import { useSession, signOut } from "next-auth/react";
+import { useRecoilState } from "recoil";
+import { userState } from "../atom/userAtom";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function MiniProfile() {
-  const { data: session } = useSession();
+  const [currentUser, setCurrentUser] = useRecoilState(userState);
+  const auth = getAuth();
+
+  const onSignOut = () => {
+    signOut(auth);
+    setCurrentUser(null);
+  };
 
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="h-16 rounded-full border p-[2px]"
-        src={session?.user.image}
+        src={currentUser?.userImg}
         alt="user-image"
       />
       <div className="flex-1 ml-4">
-        <h2 className="font-bold">{session?.user.username}</h2>
+        <h2 className="font-bold">{currentUser?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+      <button
+        onClick={onSignOut}
+        className="font-semibold text-blue-400 text-sm"
+      >
         Sign Out
       </button>
     </div>
